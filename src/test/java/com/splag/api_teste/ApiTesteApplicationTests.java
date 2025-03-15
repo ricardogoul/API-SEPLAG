a2b65e61ed1c05c5d930aa415c8b3d9d2932ea74
Ricardo de Oliveira Goulart <ricardogoul.si@gmail.com>
2025-03-15 09:58:57 -0400

    projeto base

---
+++ src/test/java/com/splag/api_teste/ApiTesteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.splag.api_teste;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiTesteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
